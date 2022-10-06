004dfee6734292eb77e6c69e2e280d0f3d2d8164
Brandon de Jesús Lara Ruiz <74059945+BrandonJLR-UTAGS@users.noreply.github.com>
2022-10-06 09:57:42 -0500

    Manifest
    se agregan manifest,sw, main

---
+++ main.js
@@ -0,0 +1,9 @@
+//service worker
+if('serviceWorker'in navigator){
+    console.log('Puedes usar los servicios en el navegador');
+    navigator.serviceWorker.register('./sw.js')
+    .then(res=>console.log('serviceWorker cargado correctamente',res))
+    .cath(err=>console.log('serviceWorker no se pudo registrar',err))
+}else{
+    console.log('no se puede iniciar el serviceworker');
+}
